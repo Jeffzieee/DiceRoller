3bd66c81a50fc50d82ed5e5c98cd94463ac1d891
Jephin John <jephinjohn85@gmail.com>
2021-07-29 17:09:57 +0530

    Defined rollDIce() function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Button Defined
-        val b1 = findViewById<Button>(R.id.b1)
+        val b1 = findViewById<Button>(R.id.roll_button)
         b1.setOnClickListener{
             Toast.makeText(applicationContext,"Rolling",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
+    }
+
+    open fun rollDice() {
+      val t1 = findViewById<TextView>(R.id.result_text)
+        var random = Random().nextInt(6) + 1
+        t1.text=random.toString()
     }
 }
