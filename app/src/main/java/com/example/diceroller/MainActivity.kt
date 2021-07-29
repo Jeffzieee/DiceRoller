19c9ac4b81ed8b325ed53e0bbfdd221db572587d
Jephin John <jephinjohn85@gmail.com>
2021-07-29 21:17:56 +0530

    lateinit Processing Efficiency

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,11 +6,13 @@ import android.widget.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dice : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Button Defined
         val b1 = findViewById<Button>(R.id.roll_button)
+        dice = findViewById<ImageView>(R.id.dice_image)
         b1.setOnClickListener{
             Toast.makeText(applicationContext,"Rolling",Toast.LENGTH_SHORT).show()
             rollDice()
@@ -18,11 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     }
      //function to rollDice
-    open fun rollDice() {
-        val dice = findViewById<ImageView>(R.id.dice_image)
+     fun rollDice() {
          var random = Random().nextInt(6)+1
-         var status = when(random){
+         var current = when(random){
              1->R.drawable.dice_1
+
              2->R.drawable.dice_2
              3->R.drawable.dice_3
              4->R.drawable.dice_4
@@ -30,6 +32,6 @@ class MainActivity : AppCompatActivity() {
              6->R.drawable.dice_6
              else ->R.drawable.empty_dice
          }
-         dice.setImageResource(status)
+         dice.setImageResource(current)
     }
 }
