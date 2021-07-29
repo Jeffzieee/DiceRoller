0ba42f6068af56caa1e52302971c26df51fd2095
Jeffzieee <jephinjohn85@gmail.com>
2021-07-29 15:54:43 +0530

    Defined Button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val b1 = findViewById<Button>(R.id.b1)
+        b1.text="GO!"
     }
 }
